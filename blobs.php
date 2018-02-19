9f242b1e603d2bb8fdc3fddcb5f42b9c2bfa62b0
DanielD3V <danieldevofficial@gmail.com>
2018-02-19 20:38:43 +0100

    forgot <br>

--- blobs.php
+++ blobs.php
@@ -1,7 +1,7 @@
 <?php
 {
 foreach (glob("*.shsh2") as $filename) { 
-echo str_replace("","","<a href='$filename' download>$filename</a>\n");
+echo str_replace("","","<a href='$filename' download>$filename</a><br>");
 }
 }
 ?>
