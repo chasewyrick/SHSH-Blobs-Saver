b8b6257ace1f9153b15a0dbc13e7d575a523878c
DanielD3V <danieldevofficial@gmail.com>
2018-02-19 22:44:55 +0100

    Added UI

--- get.php
+++ get.php
@@ -35,15 +35,51 @@ if (isset($_POST['boardconfig']) && trim($_POST['boardconfig']) != '') {
 <html lang="en"> 
 <head>
 	<meta name = "viewport" content = "width = device-width">
+	<style>
+	body{
+		background-color: #eeeeee;
+		font-family: arial;
+		margin: 0;
+	}
+h1{
+	font-family: arial;
+	font-weight: normal;
+}
+
+.whitexd{
+width: 100%;
+background-color: white;
+padding: 10px 0px 0px 0px; 
+line-height: 1.6;
+right: 3px;
+
+}
+
+a{
+	color: black;
+	text-decoration: none;
+}
+
+button{
+	width: 100%;
+	height: 50px;
+	border: none;
+	background-color: white;	
+	font-size: 13px;
+}
+</style>
 </head>
 <body>
 
+<center><h1>SHSH Saver</h1></center><br>
+<div class="whitexd">
+&nbsp;Device: <?php echo $device;?><br>
+&nbsp;Version: <?php echo $version;?><br>
+&nbsp;Boardconfig: <?php echo $boardconfig;?><br>
+&nbsp;ECID: <?php echo $ecid;?></div>
 
-Device: <?php echo $device;?><br>
-Version: <?php echo $version;?><br>
-Boardconfig: <?php echo $boardconfig;?><br>
-ECID: <?php echo $ecid;?><br>
-<a href="SHSH/<?php echo $ecid;?>/blobs.php">Get your blobs</a>
+<div class="whitexd"></div><br>
+<center><a href="SHSH/<?php echo $ecid;?>/blobs.php"><button>Get your blobs</button></a></center>
 <br>
 
 
@@ -52,7 +88,7 @@ ECID: <?php echo $ecid;?><br>
  if ($validinput) {
      shell_exec('mkdir SHSH/'.escapeshellarg($ecid).'');
      shell_exec('cp -i blobs.php SHSH/'.escapeshellarg($ecid).'');
-     shell_exec('./tsschecker_linux -d '.escapeshellarg($device).' -i '.escapeshellarg($version).' -e '.escapeshellarg($ecid).' —boardconfig '.escapeshellarg($boardconfig).' -s --save-path SHSH/'.escapeshellarg($ecid).'');
+     shell_exec('./tsschecker_macos -d '.escapeshellarg($device).' -i '.escapeshellarg($version).' -e '.escapeshellarg($ecid).' —boardconfig '.escapeshellarg($boardconfig).' -s --save-path SHSH/'.escapeshellarg($ecid).'');
  } else {
      echo '<span style="color:red;">Invalid parameters.</span>';
  }
--- index.php
+++ index.php
@@ -1,7 +1,55 @@
-<form action="get.php" method="post">
-Device: <input type="text" name="device" />
-ECID: <input type="text" name="ecid" />
-Boardconfig: <input type="text" name="boardconfig" />
-Version: <input type="text" name="version" />
-<input type="submit" value="Get BLOBS" />
-</form>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SHSH Saver</title>
+    <meta name="viewport" content="width = device-width">
+
+    <style>
+        body {
+            background-color: #eeeeee;
+            margin: 0;
+        }
+        
+        input {
+            width: 100%;
+            height: 50px;
+            font-size: 13px;
+            border: none;
+            background-color: white;
+        }
+        
+        button {
+            width: 100%;
+            height: 50px;
+            border: none;
+            font-size: 15px;
+            background-color: white;
+        }
+        
+        h1 {
+            font-family: arial;
+            font-weight: normal;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>SHSH Saver</h1>
+        <form action="get.php" method="post">
+            <input type="text" name="device" value="Device" />
+            <br>
+            <input type="text" name="ecid" value="ECID" />
+            <br>
+            <input type="text" name="boardconfig" value="Boardconfig" />
+            <br>
+            <input type="text" name="version" value="Version" />
+            <br>
+            <br>
+            <button type="submit" />Get Blobs</form>
+        </form>
+    </center>
+</body>
+
+</html>
